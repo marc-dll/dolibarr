b07dd39891a790c240501857045f0397f02698dc
Marc de Lima Lucio <68746600+marc-dll@users.noreply.github.com>
2025-07-10 09:23:50 +0200

    FIX: pgsql: boolean fields introduced in v19

--- htdocs/install/mysql/migration/18.0.0-19.0.0.sql
+++ htdocs/install/mysql/migration/18.0.0-19.0.0.sql
@@ -233,4 +233,17 @@ UPDATE llx_menu SET url = CONCAT(url, '&mode=init') WHERE fk_mainmenu = 'ticket'
 -- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN disposal_subject_to_vat DROP DEFAULT, ALTER COLUMN disposal_subject_to_vat TYPE boolean USING disposal_subject_to_vat::integer <> 0, ALTER COLUMN disposal_subject_to_vat SET DEFAULT false;
 
 -- VMYSQL4.1 ALTER TABLE llx_asset_depreciation_options_economic MODIFY COLUMN accelerated_depreciation_option boolean DEFAULT false;
--- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN llx_asset_depreciation_options_economic DROP DEFAULT, ALTER COLUMN llx_asset_depreciation_options_economic TYPE boolean USING llx_asset_depreciation_options_economic::integer <> 0, ALTER COLUMN llx_asset_depreciation_options_economic SET DEFAULT false;
+-- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN accelerated_depreciation_option DROP DEFAULT, ALTER COLUMN accelerated_depreciation_option TYPE boolean USING accelerated_depreciation_option::integer <> 0, ALTER COLUMN accelerated_depreciation_option SET DEFAULT false;
+
+
+-- VMYSQL4.1 ALTER TABLE llx_asset MODIFY COLUMN not_depreciated tinyint DEFAULT 0;
+-- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN not_depreciated DROP DEFAULT, ALTER COLUMN not_depreciated TYPE smallint USING CASE not_depreciated WHEN 't' THEN 1 ELSE 0 END, ALTER COLUMN not_depreciated SET DEFAULT 0;
+
+-- VMYSQL4.1 ALTER TABLE llx_asset MODIFY COLUMN disposal_depreciated tinyint DEFAULT 0;
+-- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN disposal_depreciated DROP DEFAULT, ALTER COLUMN disposal_depreciated TYPE smallint USING CASE disposal_depreciated WHEN 't' THEN 1 ELSE 0 END, ALTER COLUMN disposal_depreciated SET DEFAULT 0;
+
+-- VMYSQL4.1 ALTER TABLE llx_asset MODIFY COLUMN disposal_subject_to_vat tinyint DEFAULT 0;
+-- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN disposal_subject_to_vat DROP DEFAULT, ALTER COLUMN disposal_subject_to_vat TYPE smallint USING CASE disposal_subject_to_vat WHEN 't' THEN 1 ELSE 0 END, ALTER COLUMN disposal_subject_to_vat SET DEFAULT 0;
+
+-- VMYSQL4.1 ALTER TABLE llx_asset_depreciation_options_economic MODIFY COLUMN accelerated_depreciation_option tinyint DEFAULT 0;
+-- VPGSQL8.2 ALTER TABLE llx_asset ALTER COLUMN accelerated_depreciation_option DROP DEFAULT, ALTER COLUMN accelerated_depreciation_option TYPE smallint USING CASE accelerated_depreciation_option WHEN 't' THEN 1 ELSE 0 END, ALTER COLUMN accelerated_depreciation_option SET DEFAULT 0;
